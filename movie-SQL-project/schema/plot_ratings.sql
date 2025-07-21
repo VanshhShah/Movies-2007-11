c1129d9c3ac7918ee7f874718404da8527cbfd44
VanshhShah <166142414+VanshhShah@users.noreply.github.com>
2025-07-21 15:46:15 +0530

    Add files via upload

---
+++ movie-SQL-project/schema/plot_ratings.sql
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('rating.csv')
+plt.hist(df['rating'], bins=20, color='skyblue', edgecolor='black')
+plt.title('Movie Ratings Distribution')
+plt.xlabel('Rating')
+plt.ylabel('Number of Movies')
+plt.savefig('ratings_distribution.png')
+plt.show()
