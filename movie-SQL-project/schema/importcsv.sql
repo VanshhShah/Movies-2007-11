0b6f407268123bafa53c4e74a1714fa09b9969ca
VanshhShah <166142414+VanshhShah@users.noreply.github.com>
2025-07-21 15:33:37 +0530

    Create importcsv.sql

---
+++ movie-SQL-project/schema/importcsv.sql
@@ -0,0 +1,23 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect('movies.db')
+
+# Import title table
+
+df_title = pd.read_csv('movie.csv')
+df_title.to_sql('title', conn, if_exists='replace', index=False)
+
+# Import ratings table
+
+df_ratings = pd.read_csv('rating.csv')
+df_ratings.to_sql('ratings', conn, if_exists='replace', index=False)
+
+# Import names table
+df_names = pd.read_csv('names.csv')
+df_names.to_sql('names', conn, if_exists='replace', index=False)
+
+# Import crew table
+df_crew = pd.read_csv('crew.csv')
+df_crew.to_sql('crew', conn, if_exists='replace', index=False)
+conn.close()
